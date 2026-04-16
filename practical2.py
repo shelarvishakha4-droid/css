501b276534e689e46fdcacd6365910a540dfbae9
shelarvishakha4-droid <shelarvishakha4@gmail.com>
2026-04-16 01:16:11 +0000

    hi

--- practical2.py
+++ practical2.py
@@ -1,1 +1,23 @@
-print("Hello")
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Python Script - Welcome</title>
+
+    <!-- PyScript CSS -->
+    <link rel="stylesheet" href="https://pyscript.net/releases/2025.8.1/core.css">
+
+    <!-- PyScript JS (IMPORTANT) -->
+    <script type="module" src="https://pyscript.net/releases/2025.8.1/core.js"></script>
+</head>
+<body>
+
+    <h2>Python Output using PyScript</h2>
+
+    <py-script>
+        print("Welcome")
+    </py-script>
+
+</body>
+</html>
